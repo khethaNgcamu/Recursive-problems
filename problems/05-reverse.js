b831e8df8c5df2559b87953fdb3fdac198358af4
Smanga Ngcamu <ngcamukhetha@gamil.com>
2024-03-11 10:28:17 +0200

    question 5 completed

--- problems/05-reverse.js
+++ problems/05-reverse.js
@@ -13,6 +13,13 @@ reverse(""); // ""
 ***********************************************************************/
 
 // your code here
+function reverse(string){
+  if(string.length <= 1){
+    return string;
+  }
+
+  return string.slice(-1) + reverse(string.slice(0, -1));
+}
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
